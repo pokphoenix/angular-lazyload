98df59e4a3ff5eaf44f16e4d95cac6497ceb6c1b
komsan.kr <komsan.kr.18@gmail.com>
2019-10-21 15:57:14 +0700

    @add(pok) -add validate selected

--- src/app/modules/forms/components/form-tdf/form-tdf.component.ts
+++ src/app/modules/forms/components/form-tdf/form-tdf.component.ts
@@ -10,10 +10,18 @@ export class FormTdfComponent implements OnInit {
   topics = ['Angular','React','Vue'];
 
   userModel = new User("","pok@mail.com",555,"Angular","morning",true);
-
+  topicHasError = true;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  validateTopic(value){
+    if(value==='default'){
+      this.topicHasError = true;
+    }else{
+      this.topicHasError = false;
+    }
   }
 
 }
