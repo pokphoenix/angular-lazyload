7acc288aa47cdeb96dd47771d6111f91f234b728
komsan.kr <komsan.kr.18@gmail.com>
2019-10-25 11:58:55 +0700

    @add(pok) -scroll to top when change route

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -34,4 +34,18 @@ export class AppComponent implements OnInit {
     
   }
 
+  onActivate(){
+    //window.scroll(0,0);
+    //or document.body.scrollTop = 0;
+    //or document.querySelector('body').scrollTo(0,0)
+    let scrollToTop = window.setInterval(() => {
+        let pos = window.pageYOffset;
+        if (pos > 0) {
+            window.scrollTo(0, pos - 20); // how far to scroll on each step
+        } else {
+            window.clearInterval(scrollToTop);
+        }
+    }, 16);
+  }
+
 }
