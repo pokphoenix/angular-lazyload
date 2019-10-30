6389a921a3fa2aa3ae69a6f842902da97ad546ac
komsan.kr <komsan.kr.18@gmail.com>
2019-10-30 09:55:02 +0700

    @change(pok) -change error message component

--- src/app/components/error-message/error-message.component.ts
+++ src/app/components/error-message/error-message.component.ts
@@ -10,12 +10,30 @@ import { ValidationService } from 'src/app/services/validate/validation.service'
 export class ErrorMessageComponent implements OnInit {
   @Input("control") control: FormControl;
 
-  constructor() { }
+  msg;
+
+  constructor() {}
 
   ngOnInit() {
   }
 
-  get errorMessage() {
+  //Called during every change detection run, immediately after ngOnChanges() and ngOnInit().
+  ngDoCheck(){
+    this.msg = this.getErrorMessage();
+  }
+
+  // use this method  for use inside html it will call validate message all time  (but it call 2 time per use 1 time)
+  // get errorMessage() {
+  //   for (let propertyName in this.control.errors) {
+  //     if (this.control.errors.hasOwnProperty(propertyName)) {
+  //       return ValidationService.getValidatorErrorMessage(propertyName, this.control.errors[propertyName]);
+  //     }
+  //   }    
+  //   return null;
+  // }
+
+  // this method will call one time  but can update realtime when use in  ngDoCheck
+  getErrorMessage(){
     for (let propertyName in this.control.errors) {
       if (this.control.errors.hasOwnProperty(propertyName)) {
         return ValidationService.getValidatorErrorMessage(propertyName, this.control.errors[propertyName]);
