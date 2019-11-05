7f79c4d969ebcee2d5d1df804843fe9d2789b472
komsan.kr <komsan.kr.18@gmail.com>
2019-11-05 10:39:03 +0700

    @add(pok) - learning how to use switchmap for call api when input change

--- src/app/modules/forms/components/form-normal/form-normal.component.ts
+++ src/app/modules/forms/components/form-normal/form-normal.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { fromEvent } from 'rxjs';
+import { map, filter, distinctUntilChanged, debounceTime, switchMap } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-form-normal',
@@ -12,9 +15,41 @@ export class FormNormalComponent implements OnInit {
 
   debug:any ="";
 
-  constructor() { }
+  constructor(private _http:HttpClient) { }
 
   ngOnInit() {
+    const repositoryTextInput = document.querySelector('#firstName');
+    const repositoriesElement = document.querySelector('#repositories');
+
+    const SEARCH_URL = 'https://api.github.com/search/repositories?q=';
+
+    const repositoryNameChanges$ = fromEvent(repositoryTextInput, 'input').pipe(
+      map(e=>e.target['value'])
+    );
+
+    
+
+    const repositories$ = repositoryNameChanges$.pipe(
+      filter(Boolean),
+      distinctUntilChanged(),
+      debounceTime(1000),
+      switchMap((name) => this.formFetch(SEARCH_URL + name)),
+      // switchMap((response) => response.json())
+    );
+
+    repositories$.subscribe((repos) => {
+      //  console.log(repos);
+    const elements = repos ? repos.items.reduce((acc, repo) => {
+        return acc + `<div>${repo.name}</div>`;
+      }, '') : '';
+
+      repositoriesElement.innerHTML = elements;
+    });
+  }
+
+  formFetch(url){
+    console.log(url)
+    return this._http.get<any>(url);
   }
 
   onSubmit(formData){
